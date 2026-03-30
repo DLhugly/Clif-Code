8f3b9b3a6d47daac069f543b905e726b2e88e091
James Lawrence <lawrencedigitalco@gmail.com>
2026-03-30 12:34:25 +0800

    fix: export missing icons from git components

--- clif-pad-ide/src/components/git/index.ts
+++ clif-pad-ide/src/components/git/index.ts
@@ -1,2 +1,3 @@
 export { default as FileRow, getStatusColor, getStatusLabel } from "./FileRow";
 export { default as GitGraphRow } from "./GitGraphRow";
+export { PlusIcon, MinusIcon, RenameIcon, DeleteIcon } from "./icons";
